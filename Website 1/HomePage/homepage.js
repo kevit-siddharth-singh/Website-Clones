7774124bedf8a938ac7ba653ca570d7df5ab3a33
kevit-siddharth-singh <siddharth.singh@kevit.io>
2024-06-19 17:15:18 +0530

    Work with us Section

--- Website 1/HomePage/homepage.js
+++ Website 1/HomePage/homepage.js
@@ -1,16 +1,31 @@
 const card = document.querySelectorAll(".card");
-
-const observer = new IntersectionObserver((items) => {
-  items.forEach((element) => {
-    element.target.classList.toggle("show", element.isIntersecting);
-  });
-},{
+const workball = document.querySelectorAll(".work-ball");
+const observer = new IntersectionObserver(
+  (items) => {
+    items.forEach((element) => {
+      element.target.classList.toggle("show", element.isIntersecting);
+    });
+  },
+  {
     threshold: 0.5,
-}
-
-
+  }
+);
+const observer2 = new IntersectionObserver(
+  (items) => {
+    items.forEach((element) => {
+      element.target.classList.toggle("show-ball", element.isIntersecting);
+    });
+  },
+  {
+    threshold: 0.5,
+  }
 );
 
-card.forEach(card => {
-        observer.observe(card);
+
+card.forEach((card) => {
+  observer.observe(card);
 });
+workball.forEach((workball) => {
+  observer2.observe(workball);
+});
+ 
