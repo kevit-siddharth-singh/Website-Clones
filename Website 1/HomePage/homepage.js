4a23f9eb83be90bc9c4cc20fee21d0887059ab7d
kevit-siddharth-singh <siddharth.singh@kevit.io>
2024-06-20 16:42:42 +0530

    Added Topup Btn

--- Website 1/HomePage/homepage.js
+++ Website 1/HomePage/homepage.js
@@ -1,5 +1,7 @@
 const card = document.querySelectorAll(".card");
 const workball = document.querySelectorAll(".work-ball");
+
+
 const observer = new IntersectionObserver(
   (items) => {
     items.forEach((element) => {
@@ -22,10 +24,42 @@ const observer2 = new IntersectionObserver(
 );
 
 
+
+// Sid:  For Cards On Scroll
 card.forEach((card) => {
   observer.observe(card);
 });
+
+// !: For Workball
 workball.forEach((workball) => {
   observer2.observe(workball);
 });
- 
+
+
+
+// Sid : Js  Logic For  Topup Btn Visibility
+// Get the button:
+let topupbtn = document.querySelector(".topup-btn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topupbtn.style.opacity = "1";
+    topupbtn.style['pointer-events'] = "auto";
+   
+
+  } else {
+    topupbtn.style.opacity = "0";
+
+  
+    topupbtn.style['pointer-events'] = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
